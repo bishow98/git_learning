69023d45fe4749cc65d1947295c4c17195433eaf
Bishow Chauhan <chauhandavid4199@gmail.com>
2021-07-16 06:42:14 +0545

    simple change in code

--- areaoftriangle.c
+++ areaoftriangle.c
@@ -3,7 +3,7 @@ int main()
 {
 	float b,h,area;
 	printf("program to calculate area of triangle")
-	printf("\a enter base and height of  triangle \n : ");
+	printf("enter base and height of  triangle \n : ");
 	scanf("%f%f",&b,&h);
 	area= 0.5*b*h;
 	printf("the area of triangle is %f",area);
